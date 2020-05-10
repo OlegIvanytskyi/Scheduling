f598dd1ad3c33352ea3e51d5a8c1de71db82e8d9
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-10 23:49:27 +0300

    add helper

---
+++ helper.py
@@ -0,0 +1,7 @@
+import tkinter
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
+FONT = ('Arial', 14)
