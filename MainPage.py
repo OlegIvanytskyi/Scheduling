cf88b603d8a28494603a3b55a53de608b4b6e8c8
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 01:04:58 +0300

    change mainpage

--- MainPage.py
+++ MainPage.py
@@ -1,50 +1,29 @@
 from helper import *
 from Graph import Graph
+from TestOneDueDate import TestOneDueDate
+from TestTwoDueDates import TestTwoDueDates
+from ResearchOneDueDate import ResearchOneDueDate
+from ResearchTwoDueDates import ResearchTwoDueDates
 
 
 class MainPage(tkinter.Frame):
     def __init__(self, parent, controller):
         tkinter.Frame.__init__(self, parent)
 
-        tkinter.Label(self, text='Parameters', font=FONT).grid(columnspan=2, pady=12)
+        tkinter.Label(self,
+                      text='Вітаємо в системі з підтримки дослідження\nалгоритмів розв\'язання задач теорії розкладів',
+                      font=font_header).grid(row=0, columnspan=2, pady=12)
 
-        tkinter.Label(self, text='Enter number of jobs: ', font=FONT).grid(row=1, column=0, pady=5)
-        self.n = tkinter.Entry(self, font=FONT)
-        self.n.grid(row=1, column=1, pady=5)
+        tkinter.Label(self, text='Тестування', font=font_bold).grid(row=1, columnspan=2, pady=12)
 
-        # tkinter.Label(self, text='Enter number of machines: ', font=FONT).grid(row=2, column=0, pady=5)
-        # self.m = tkinter.Entry(self, font=FONT)
-        # self.m.grid(row=2, column=1, pady=5)
+        tkinter.Button(self, text='Алгоритми з одним директивним строком', font=font,
+                       command=lambda: controller.frames[TestOneDueDate].tkraise()).grid(row=2, column=0, sticky='nwes')
+        tkinter.Button(self, text='Алгоритми з двома директивними строками', font=font,
+                       command=lambda: controller.frames[TestTwoDueDates].tkraise()).grid(row=2, column=1, sticky='nwes')
 
-        size_arr = ['S', 'M', 'L']
+        tkinter.Label(self, text='Дослідження', font=font_bold).grid(row=3, columnspan=2, pady=12)
 
-        tkinter.Label(self, text='Choose the size of mean due date: ', font=FONT).grid(row=3, column=0, pady=5)
-        self.seed_d_var = tkinter.StringVar()
-        self.seed_d_var.set('S')
-        self.seed_d_menu = tkinter.OptionMenu(self, self.seed_d_var, *size_arr)
-        self.seed_d_menu.config(font=FONT)
-        self.seed_d_menu.grid(row=3, column=1, sticky='ew', pady=5)
-
-        tkinter.Label(self, text='Choose the size of deviation of due date: ', font=FONT).grid(row=4, column=0, pady=5)
-        self.dev_d_var = tkinter.StringVar()
-        self.dev_d_var.set('S')
-        self.dev_d_menu = tkinter.OptionMenu(self, self.dev_d_var, *size_arr)
-        self.dev_d_menu.config(font=FONT)
-        self.dev_d_menu.grid(row=4, column=1, sticky='ew', pady=5)
-
-        tkinter.Label(self, text='Choose the size of mean p: ', font=FONT).grid(row=5, column=0, pady=5)
-        self.seed_p_var = tkinter.StringVar()
-        self.seed_p_var.set('S')
-        self.seed_p_menu = tkinter.OptionMenu(self, self.seed_p_var, *size_arr)
-        self.seed_p_menu.config(font=FONT)
-        self.seed_p_menu.grid(row=5, column=1, sticky='ew', pady=5)
-
-        tkinter.Label(self, text='Choose the size of deviation of p: ', font=FONT).grid(row=6, column=0, pady=5)
-        self.dev_p_var = tkinter.StringVar()
-        self.dev_p_var.set('S')
-        self.dev_p_menu = tkinter.OptionMenu(self, self.dev_p_var, *size_arr)
-        self.dev_p_menu.config(font=FONT)
-        self.dev_p_menu.grid(row=6, column=1, sticky='ew', pady=5)
-
-        tkinter.Button(self, text='Submit', font=FONT,
-                       command=lambda: controller.frames[Graph].draw_graph().tkraise()).grid(columnspan=2, pady=12)
+        tkinter.Button(self, text='Системи з одним директивним строком', font=font,
+                       command=lambda: controller.frames[ResearchOneDueDate].tkraise()).grid(row=4, column=0, sticky='nwes')
+        tkinter.Button(self, text='Системи з двома директивними строками', font=font,
+                       command=lambda: controller.frames[ResearchTwoDueDates].tkraise()).grid(row=4, column=1, sticky='nwes')
