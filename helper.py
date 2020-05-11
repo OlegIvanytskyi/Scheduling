a9f769f614554a59daa99e43775c21d7729e5014
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 01:05:12 +0300

    add fonts

--- helper.py
+++ helper.py
@@ -4,4 +4,6 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 
-FONT = ('Arial', 14)
+font = ('Arial', 14)
+font_bold = ('Arial', 14, 'bold')
+font_header = ('Arial', 16, 'bold')
