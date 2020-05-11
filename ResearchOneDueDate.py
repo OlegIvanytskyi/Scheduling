67dcdbff2aeb04c744a1950f27bc62abf00666e8
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 01:06:41 +0300

    add file for research with one due date

---
+++ ResearchOneDueDate.py
@@ -0,0 +1,12 @@
+from helper import *
+from Graph import Graph
+
+
+class ResearchOneDueDate(tkinter.Frame):
+    def __init__(self, parent, controller):
+        tkinter.Frame.__init__(self, parent)
+
+        tkinter.Label(self,
+                      text='Вітаємо в системі з підтримки дослідження\nалгоритмів розв\'язання задач теорії розкладів',
+                      font=font_header).grid(row=0, columnspan=2, pady=12)
+
