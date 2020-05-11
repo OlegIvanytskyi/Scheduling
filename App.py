d1c31442c3b23589cfb77ebfa22e10f9398a8093
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 01:04:39 +0300

    add frames

--- App.py
+++ App.py
@@ -1,6 +1,10 @@
 from helper import *
 from MainPage import MainPage
 from Graph import Graph
+from TestOneDueDate import TestOneDueDate
+from TestTwoDueDates import TestTwoDueDates
+from ResearchOneDueDate import ResearchOneDueDate
+from ResearchTwoDueDates import ResearchTwoDueDates
 
 
 class App(tkinter.Tk):
@@ -8,7 +12,7 @@ class App(tkinter.Tk):
         tkinter.Tk.__init__(self, *args, **kwargs)
 
         tkinter.Tk.wm_title(self, 'Scheduling')
-        tkinter.Tk.geometry(self, '1000x500')
+        tkinter.Tk.geometry(self, '775x300')
 
         container = tkinter.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -19,6 +23,18 @@ class App(tkinter.Tk):
 
         self.frames[MainPage] = MainPage(container, self)
         self.frames[MainPage].grid(row=0, column=0, sticky='nsew')
+
+        self.frames[TestOneDueDate] = TestOneDueDate(container, self)
+        self.frames[TestOneDueDate].grid(row=0, column=0, sticky='nsew')
+
+        self.frames[TestTwoDueDates] = TestTwoDueDates(container, self)
+        self.frames[TestTwoDueDates].grid(row=0, column=0, sticky='nsew')
+
+        self.frames[ResearchOneDueDate] = ResearchOneDueDate(container, self)
+        self.frames[ResearchOneDueDate].grid(row=0, column=0, sticky='nsew')
+
+        self.frames[ResearchTwoDueDates] = ResearchTwoDueDates(container, self)
+        self.frames[ResearchTwoDueDates].grid(row=0, column=0, sticky='nsew')
 
         self.frames[Graph] = Graph(container, self)
         self.frames[Graph].grid(row=0, column=0, sticky='nsew')
