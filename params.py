97889ce1827a21d42017c12f8929d0786416be43
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 18:02:36 +0300

    chane helper to params

--- params.py
+++ params.py
@@ -7,3 +7,8 @@ from matplotlib.figure import Figure
 font = ('Arial', 14)
 font_bold = ('Arial', 14, 'bold')
 font_header = ('Arial', 16, 'bold')
+
+blue_color = '#0066cc'
+light_grey_color = '#E4E4EB'
+dark_grey_color = '#353C4E'
+
