f103898146433b5ea33f5ac4015fdba069b69ca6
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 12:48:43 +0300

    different buttons

--- MainPage.py
+++ MainPage.py
@@ -1,7 +1,6 @@
 from helper import *
 from Graph import Graph
-from TestOneDueDate import TestOneDueDate
-from TestTwoDueDates import TestTwoDueDates
+from Test import Test
 from ResearchOneDueDate import ResearchOneDueDate
 from ResearchTwoDueDates import ResearchTwoDueDates
 
@@ -14,16 +13,18 @@ class MainPage(tkinter.Frame):
                       text='Вітаємо в системі з підтримки дослідження\nалгоритмів розв\'язання задач теорії розкладів',
                       font=font_header).grid(row=0, columnspan=2, pady=12)
 
-        tkinter.Label(self, text='Тестування', font=font_bold).grid(row=1, columnspan=2, pady=12)
+        self.grid_rowconfigure(1, minsize=30)
 
-        tkinter.Button(self, text='Алгоритми з одним директивним строком', font=font,
-                       command=lambda: controller.frames[TestOneDueDate].tkraise()).grid(row=2, column=0, sticky='nwes')
-        tkinter.Button(self, text='Алгоритми з двома директивними строками', font=font,
-                       command=lambda: controller.frames[TestTwoDueDates].tkraise()).grid(row=2, column=1, sticky='nwes')
+        tkinter.Button(self, text='Тестування', font=font,
+                       command=lambda: controller.frames[Test].tkraise()).grid(row=2, columnspan=2)
 
-        tkinter.Label(self, text='Дослідження', font=font_bold).grid(row=3, columnspan=2, pady=12)
+        self.grid_rowconfigure(3, minsize=30)
+
+        tkinter.Label(self, text='Дослідження', font=font_bold).grid(row=4, columnspan=2, pady=12)
 
         tkinter.Button(self, text='Системи з одним директивним строком', font=font,
-                       command=lambda: controller.frames[ResearchOneDueDate].tkraise()).grid(row=4, column=0, sticky='nwes')
+                       command=lambda: controller.frames[ResearchOneDueDate].tkraise()).grid(row=5, column=0,
+                                                                                             sticky='nwes')
         tkinter.Button(self, text='Системи з двома директивними строками', font=font,
-                       command=lambda: controller.frames[ResearchTwoDueDates].tkraise()).grid(row=4, column=1, sticky='nwes')
+                       command=lambda: controller.frames[ResearchTwoDueDates].tkraise()).grid(row=5, column=1,
+                                                                                              sticky='nwes')
