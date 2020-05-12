3fbef3e66d69d94d7dcd406d487e129ae26c09d4
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-12 12:49:07 +0300

    create file for testing window

---
+++ Test.py
@@ -0,0 +1,33 @@
+from helper import *
+from tkinter.filedialog import askopenfilename
+
+
+class Test(tkinter.Frame):
+    def __init__(self, parent, controller):
+        tkinter.Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.button = tkinter.Button(self, text='Назад', font=font, command=lambda: self.reset_frame()).place(x=0, y=0)
+
+        tkinter.Button(self, text='Оберіть файл з вхідними даними', font=font,
+                       command=lambda: self.get_filename()).pack(pady=25)
+
+        self.literal = tkinter.Label(self, text='Обраний файл', font=font)
+
+        self.text = tkinter.StringVar()
+        self.text.set('')
+        self.label = tkinter.Label(self, textvariable=self.text, font=font, borderwidth=3, relief="groove", pady=20)
+
+        self.button = tkinter.Button(self, text='Продовжити', font=font, command=lambda: self.get_filename())
+
+    def get_filename(self):
+        filename = askopenfilename()
+
+        self.literal.pack(pady=10)
+        self.label.pack()
+        self.text.set(filename)
+        self.button.pack(pady=20)
+
+    def reset_frame(self):
+        from MainPage import MainPage
+        self.controller.frames[MainPage].tkraise()
