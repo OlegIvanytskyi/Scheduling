e263cfdeb1ab26d9673dd3794a5eecb4f5fd8182
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-05-13 15:51:03 +0300

    add several features to graph (like due date and annotations)

--- Gantt.py
+++ Gantt.py
@@ -29,7 +29,7 @@ class Gantt():
         gnt.set_title('Побудований розклад')
 
         gnt.set_ylim(0, (self.num_machines + 1) * 10 + 10)
-        gnt.set_xlim(0, max([sum([job[1] for job in machine]) for machine in schedule]) + 5)  # max of all schedules+10
+        gnt.set_xlim(0, max([sum([job[1] for job in machine]) for machine in schedule]) + 5)  # max of all schedules+10 # todo
 
         gnt.set_xlabel('Час')
         gnt.set_ylabel('Машина')
@@ -41,9 +41,23 @@ class Gantt():
         gnt.set_yticks(yticks)
         gnt.set_yticklabels(yticklabels)
 
+        # setting the schedules
         for i in range(self.num_machines):
             gnt.broken_barh(schedule[i], ((i + 1) * 10, 9),
                             facecolors=('orange', 'green', 'blue') * len(schedule),  # mark each job with diff color
                             edgecolors='black')  # color of borders
+            for job in schedule[i]:
+                gnt.text(x=job[0] + job[1] / 2,  # x = gorizontal position of the label (center of the bar)
+                         y=(i + 1) * 10 + 4,  # y = vertical position of the label
+                         s=job[1],  # text (length of the job)
+                         ha='center', va='center', color='white', size=15)
+
+        # setting the due dates
+        for i in range(self.num_d):
+            gnt.axvline(x=self.d[i], c='black', linewidth=5)
+            # gnt.text(x=self.d[i] + 0.5, y=1, s=f'd = {self.d[i]}', color='black', size=10)  # possible way to
+                                                                                              # set xtick for due date
+
+        gnt.set_xticks(list(gnt.get_xticks()) + self.d)  # set xtick for due date
 
         fig.show()
