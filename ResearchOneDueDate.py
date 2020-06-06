56b7118ca4558d27be94725341a0a7e8198ee5c1
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-06-07 01:38:39 +0300

    window ready

--- ResearchOneDueDate.py
+++ ResearchOneDueDate.py
@@ -14,35 +14,35 @@ class ResearchOneDueDate(tkinter.Frame):
 
         tkinter.Label(self, text='Параметри генератора', font=font_header).grid(row=1, columnspan=2, padx=275, pady=50)
 
-        tkinter.Label(self, text='Розмір директивного строку', font=font).grid(row=2, column=0, padx=20, pady=10)
+        tkinter.Label(self, text='Кількість робіт', font=font).grid(row=2, column=0, padx=20, pady=10, sticky='ew')
         self.d_var = tkinter.StringVar()
         self.d_var.set(arr[0])
         self.d_menu = tkinter.OptionMenu(self, self.d_var, *arr)
         self.d_menu.config(font=font)
         self.d_menu.grid(row=2, column=1, padx=10, pady=10, sticky='w')
 
-        tkinter.Label(self, text='Розмір директивного строку', font=font).grid(row=3, column=0, padx=20, pady=10)
+        tkinter.Label(self, text='Розмір директивного строку', font=font).grid(row=3, column=0, padx=20, pady=10, sticky='ew')
         self.d_var = tkinter.StringVar()
         self.d_var.set(arr[0])
         self.d_menu = tkinter.OptionMenu(self, self.d_var, *arr)
         self.d_menu.config(font=font)
         self.d_menu.grid(row=3, column=1, padx=10, pady=10, sticky='w')
 
-        tkinter.Label(self, text='Розмір директивного строку', font=font).grid(row=4, column=0, padx=20, pady=10)
+        tkinter.Label(self, text='Розміри робіт', font=font).grid(row=4, column=0, padx=20, pady=10, sticky='ew')
         self.d_var = tkinter.StringVar()
         self.d_var.set(arr[0])
         self.d_menu = tkinter.OptionMenu(self, self.d_var, *arr)
         self.d_menu.config(font=font)
         self.d_menu.grid(row=4, column=1, padx=10, pady=10, sticky='w')
 
-        tkinter.Label(self, text='Розмір директивного строку', font=font).grid(row=5, column=0, padx=20, pady=10)
+        tkinter.Label(self, text='Кількість машин', font=font).grid(row=5, column=0, padx=20, pady=10, sticky='ew')
         self.d_var = tkinter.StringVar()
         self.d_var.set(arr[0])
         self.d_menu = tkinter.OptionMenu(self, self.d_var, *arr)
         self.d_menu.config(font=font)
         self.d_menu.grid(row=5, column=1, padx=10, pady=10, sticky='w')
 
-        tkinter.Label(self, text='Розмір директивного строку', font=font).grid(row=6, column=0, padx=20, pady=10)
+        tkinter.Label(self, text='Продуктивності машин', font=font).grid(row=6, column=0, padx=20, pady=10, sticky='ew')
         self.d_var = tkinter.StringVar()
         self.d_var.set(arr[0])
         self.d_menu = tkinter.OptionMenu(self, self.d_var, *arr)
@@ -53,10 +53,6 @@ class ResearchOneDueDate(tkinter.Frame):
                        command=lambda: self.kek()).grid(row=7, column=0, pady=50)
         tkinter.Button(self, text='Повне дослідження', font=font_bold,
                        command=lambda: self.kek()).grid(row=7, column=1, pady=50, sticky='w')
-
-        #
-        # tkinter.Button(self, text='Submit', font=font,
-        #                command=lambda: controller.frames[Gantt].draw_graph().tkraise()).grid(columnspan=2, pady=12)
 
     def reset_frame(self):
         from MainPage import MainPage
